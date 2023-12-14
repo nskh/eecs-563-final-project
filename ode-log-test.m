63cb7757ee09229c5668c3ec1bde104c4691664e
Nishant Kheterpal <nishantkheterpal@gmail.com>
2023-12-14 14:25:21 -0500

    One-step noise, different initial condition, and more plots

--- ode-log-test.m
+++ ode-log-test.m
@@ -2,21 +2,40 @@
 A = [-7  1; 
      8  -10]; % 10.9
 
-A = [-1 1; -1 -1]; % 11.15
+% A = [-1 1; -1 -1]; % 11.15
 
 eig(A)
-errormag = 1;
-tin = [0, 10];
-y0 = [0, 0];
+errormag = 1000;
+t_disturbance = 2;
+tin = [0:0.1:10];
+y0 = [10, 10];
 
-odeFcn = @(t, y) linear_dyn(t, y, A, errormag);
+odeFcn = @(t, y) noisy_linear_dyn(t, y, A, errormag, t_disturbance);
 [timeout,out] = ode45(odeFcn, tin, y0);
 
+figure
 plot(timeout, out)
 title(sprintf("Trajectory for error %.1f", errormag))
 xlabel("time")
 ylabel("state value")
-legend(["y_0", "y_1"])
+
+hold on
+
+odeFcn = @(t, y) A * y;
+[timeout_noiseless,out_noiseless] = ode45(odeFcn, tin, y0);
+
+% figure
+plot(timeout_noiseless, out_noiseless)
+% title("Noiseless Trajectory")
+% xlabel("time")
+% ylabel("state value")
+legend(["noisy y_0", "noisy y_1", "noiseless y_0", "noiseless y_1"])
+
+figure
+title("Output errors")
+xlabel("Time")
+ylabel("Error value")
+plot(out_noiseless - out)
 
 %% Bicycle 
 v = 1;
@@ -27,4 +46,4 @@ y0 = [0, 0, theta0];
 tin = [0,1];
 odeFcn = @(t,y) bicycle_dyn(t, y, v, l);
 options = odeset('OutputFcn',@(t,y,flag) deltaOutputFn(t,y,flag, v, l));
-[timeout,out] = ode45(odeFcn, tin, y0, options);
+[timeout_noiseless,out_noiseless] = ode45(odeFcn, tin, y0, options);
